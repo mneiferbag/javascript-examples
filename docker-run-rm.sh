5db3df818c3cd9e735d301c225c0e64fbe332c99
Markus Neifer <mneiferbag@users.noreply.github.com>
2021-04-05 08:59:12 +0000

    Adjust container name

--- docker-run-rm.sh
+++ docker-run-rm.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #
-# Copyright (c) 2020 Markus Neifer
+# Copyright (c) 2020-2021 Markus Neifer
 # Licensed under the MIT License. See LICENSE file for license information.
 #
 
 docker run \
   --rm \
   -it \
-  --name mhn-node-tmp \
+  --name mneifercons-node-tmp \
   -v $PWD:/home/app \
   -w /home/app \
   -p 8080:3000  \
